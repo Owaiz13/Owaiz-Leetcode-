9a08312da9e0445a5f4c7bc73faca14bd9a0ccfa
Owaiz13 <114177684+Owaiz13@users.noreply.github.com>
2024-05-13 22:11:14 +0530

    Time: 5 ms (31.84%) | Memory: 10.3 MB (9.21%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        int res = (1<<(m-1))*n;
+        for(int i=1;i<m;i++) {
+            int curr=0;
+            for(int j=0;j<n;j++) {
+                if(a[j][i]==a[j][0])curr++;
+            }
+            res += (1<<(m-i-1))*max(curr, n-curr);
+        }
+        return res;
+    }
+};
