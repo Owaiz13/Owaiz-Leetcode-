7cd459ffd415852675b6fd8e6a704ee8280d9cbf
Owaiz13 <114177684+Owaiz13@users.noreply.github.com>
2024-07-10 11:46:42 +0530

    Time: 10 ms (7.41%) | Memory: 14 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int count=0;
-        for(int i=0;i<logs.size();i++){
-            if(logs[i]=="./"){
-                count=count;
+        stack<int>s;
+        for(auto i:logs){
+            if(i=="../"){
+                if(s.empty()){
+                    continue;
+                }
+                else{
+                    s.pop();
+                }
             }
-        else if(logs[i]=="../"){
-            count--;
-            if(count<0){
-                count=0;
+            else if(i=="./"){
+                continue;
+            }
+            else{
+                s.push(1);
             }
         }
-        else{
-            count++;
-        }
-        }
-        
-        return count;
+        return s.size();
     }
 };
